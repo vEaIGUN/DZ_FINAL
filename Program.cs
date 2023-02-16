84572e3c3407805b07ef3861668e13aa1255d226
Aigun <veaigun>
2023-02-16 13:27:12 +0400

    Added a file

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 1.
+
+System.Console.Write("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+int NaturalElem(int num)
+{
+    if (num == 1)
+    {
+        return 1;
+    }
+    System.Console.WriteLine(num);
+    return NaturalElem(num - 1);
+}
+
+System.Console.WriteLine(NaturalElem(num));
+
+
+// Задача 2.
+
+System.Console.Write("Введите число M:");
+int M = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите число N:");
+int N = Convert.ToInt32(Console.ReadLine());
+int NaturalElem(int N, int M)
+{
+    // int sum = 0;
+    if (N == M)
+    {
+        return M;
+    }
+    // sum= sum+N;
+    return (N + NaturalElem(N - 1, M));
+}
+
+System.Console.WriteLine(NaturalElem(N, M));
+
+// Задача 3.
+
+System.Console.Write("Введите число M:");
+int M = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите число N:");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int Akkerman(int M, int N)
+{
+    if (M == 0)
+    {
+        return N + 1;
+    }
+    else if (N == 0 && M > 0)
+    {
+        return Akkerman(M - 1, 1);
+    }
+    else
+    {
+        return (Akkerman(M - 1, Akkerman(M, N - 1)));
+    }
+}
+System.Console.WriteLine(Akkerman(M, N));
